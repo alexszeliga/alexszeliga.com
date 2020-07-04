db45b4ea780f4b916e95d6422ff13c9b668280f0
Alex Szeliga <alexszeliga@gmail.com>
2020-07-04 09:49:27 -0400

    add markdown file for home page, add hero image style I added a markdown file and queried it on the home page js component and also modified the hero image styles so it leaves room for the fixed nav.

---
+++ content/blog/index.md
@@ -0,0 +1,8 @@
+---
+title: Home Page
+date: "2020-07-04T22:40:32.169Z"
+description: The home page for the personal website for Alex Szeliga
+hero:
+  title: Alex Szeliga
+  subtitle: Web Developer and Technology Strategist
+---
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,13 @@ import SEO from "../components/seo"
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const heroFluid = data.heroImage.childImageSharp.fluid
+  const pageContent = data.pageContent.frontmatter
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Home Page" />
       <Hero heroFluid={heroFluid}>
-        <h1 className="title has-text-light">Alex Szeliga</h1>
-        <h2 className="subtitle has-text-light">Web Developer and Technology Strategist</h2>
+        <h1 className="title has-text-light">{pageContent.hero.title}</h1>
+        <h2 className="subtitle has-text-light">{pageContent.hero.subtitle}</h2>
       </Hero>
     </Layout>
   )
@@ -32,6 +33,17 @@ export const pageQuery = graphql`
         childImageSharp {
           fluid(quality: 100, maxWidth: 4032) {
             ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    pageContent: markdownRemark(fields: {slug:{eq:"/"}}) {
+      frontmatter {
+        date
+        description
+        title
+        hero {
+          title
+          subtitle
         }
       }
     }
