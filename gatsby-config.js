739f08e0c447fe85fd62ab1a648cd2a8c4bcfc4b
Alex Szeliga <alexszeliga@gmail.com>
2020-07-17 14:53:05 -0400

    add tracking tag to landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-172946681-1`,
+        head: true,
       },
     },
     `gatsby-plugin-feed`,
